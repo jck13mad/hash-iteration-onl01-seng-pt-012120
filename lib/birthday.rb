f0062c888f3a0d0b1c786265ec821ff079992081
Jack Madden <jck13mad@gmail.com>
2020-02-02 14:43:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,9 +13,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age > 12
+    if age =< 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    end
+    else 
+      puts ""
   end
 end
 
